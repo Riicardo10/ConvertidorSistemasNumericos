6147cfcc341f71cee20378d4fcc2c1cbe7970851
Riicardo <Riicardo@RicardoFlores>
2015-10-11 00:24:23 -0500

    Elaboración de todos los métodos para convertir a binarios, octales y hexadecimales, también la creación de un método de conversión de base diez a cualquier base menor o igual a la hexadecimal. Falta unificar todos los métodos en uno general.

--- GitConvertidorSistemasNumericos/src/gitconvertidorsistemasnumericos/Hexadecimal.java
+++ GitConvertidorSistemasNumericos/src/gitconvertidorsistemasnumericos/Hexadecimal.java
@@ -1,5 +1,78 @@
 package gitconvertidorsistemasnumericos;
 public class Hexadecimal {
+    public String binario_a_octal(String binario){
+        int tamanio = binario.length();
+        char car[] = binario.toCharArray();
+        int arreglo[] = new int[tamanio];
+        for (int i = 0; i < tamanio; i++) {
+            switch(car[i]){
+                case '1':
+                    arreglo[i] = 1;
+                    break;
+                case '0':
+                    arreglo[i] = 0;
+                    break;
+                }
+        }
+        int potencia = 0;
+        int suma = 0;
+        System.out.println("datos del arreglo:");
+        for (int j = 0; j < tamanio; j++) {
+            potencia = (j - (tamanio-1)) * -1;
+            suma += (int) (arreglo[j] * Math.pow(2, potencia));
+        }
+        Hexadecimal conv = new Hexadecimal();
+        String octal = conv.decimal_a_otro(suma, 8);
+        return octal;
+    }
+    public String binario_a_decimal(String binario){
+        int tamanio = binario.length();
+        char car[] = binario.toCharArray();
+        int arreglo[] = new int[tamanio];
+        for (int i = 0; i < tamanio; i++) {
+            switch(car[i]){
+                case '1':
+                    arreglo[i] = 1;
+                    break;
+                case '0':
+                    arreglo[i] = 0;
+                    break;
+                }
+        }
+        int potencia = 0;
+        int suma = 0;
+        System.out.println("datos del arreglo:");
+        for (int j = 0; j < tamanio; j++) {
+            potencia = (j - (tamanio-1)) * -1;
+            suma += (int) (arreglo[j] * Math.pow(2, potencia));
+        }
+        String decimal = suma + "";
+        return decimal;
+    }
+    public String binario_a_hexadecimal(String binario){
+        int tamanio = binario.length();
+        char car[] = binario.toCharArray();
+        int arreglo[] = new int[tamanio];
+        for (int i = 0; i < tamanio; i++) {
+            switch(car[i]){
+                case '1':
+                    arreglo[i] = 1;
+                    break;
+                case '0':
+                    arreglo[i] = 0;
+                    break;
+                }
+        }
+        int potencia = 0;
+        int suma = 0;
+        for (int j = 0; j < tamanio; j++) {
+            potencia = (j - (tamanio-1)) * -1;
+            suma += (int) (arreglo[j] * Math.pow(2, potencia));
+        }
+        Hexadecimal conv = new Hexadecimal();
+        String hexadecimal = conv.decimal_a_otro(suma, 16);
+        return hexadecimal;
+    }
     public String decimal_a_otro(int decimal, int base){
         String hex = "";
         int letra;
@@ -63,12 +136,30 @@ public class Hexadecimal {
         String hexadecimal = aux.reverse().toString();
         return hexadecimal;
     }
-    public String binario_a_decimal(String binario){
-        int tamanio = binario.length();
-        char car[] = binario.toCharArray();
+    public String octal_a_binario(String octal){
+        int tamanio = octal.length();
+        char car[] = octal.toCharArray();
         int arreglo[] = new int[tamanio];
         for (int i = 0; i < tamanio; i++) {
             switch(car[i]){
+                case '7':
+                    arreglo[i] = 7;
+                    break;
+                case '6':
+                    arreglo[i] = 6;
+                    break;
+                case '5':
+                    arreglo[i] = 5;
+                    break;
+                case '4':
+                    arreglo[i] = 4;
+                    break;
+                case '3':
+                    arreglo[i] = 3;
+                    break;
+                case '2':
+                    arreglo[i] = 2;
+                    break;
                 case '1':
                     arreglo[i] = 1;
                     break;
@@ -79,13 +170,13 @@ public class Hexadecimal {
         }
         int potencia = 0;
         int suma = 0;
-        System.out.println("datos del arreglo:");
         for (int j = 0; j < tamanio; j++) {
             potencia = (j - (tamanio-1)) * -1;
-            suma += (int) (arreglo[j] * Math.pow(2, potencia));
+            suma += (int) (arreglo[j] * Math.pow(8, potencia));
         }
-        String decimal = suma + "";
-        return decimal;
+        Hexadecimal conv = new Hexadecimal();
+        String hexadecimal = conv.decimal_a_otro(suma, 2);
+        return hexadecimal;
     }
     public String octal_a_decimal(String octal){
         int tamanio = octal.length();
@@ -128,6 +219,115 @@ public class Hexadecimal {
         }
         String decimal = suma + "";
         return decimal;
+    }
+    public String octal_a_hexadecimal(String octal){
+        int tamanio = octal.length();
+        char car[] = octal.toCharArray();
+        int arreglo[] = new int[tamanio];
+        for (int i = 0; i < tamanio; i++) {
+            switch(car[i]){
+                case '7':
+                    arreglo[i] = 7;
+                    break;
+                case '6':
+                    arreglo[i] = 6;
+                    break;
+                case '5':
+                    arreglo[i] = 5;
+                    break;
+                case '4':
+                    arreglo[i] = 4;
+                    break;
+                case '3':
+                    arreglo[i] = 3;
+                    break;
+                case '2':
+                    arreglo[i] = 2;
+                    break;
+                case '1':
+                    arreglo[i] = 1;
+                    break;
+                case '0':
+                    arreglo[i] = 0;
+                    break;
+                }
+        }
+        int potencia = 0;
+        int suma = 0;
+        for (int j = 0; j < tamanio; j++) {
+            potencia = (j - (tamanio-1)) * -1;
+            suma += (int) (arreglo[j] * Math.pow(8, potencia));
+        }
+        Hexadecimal conv = new Hexadecimal();
+        String hexadecimal = conv.decimal_a_otro(suma, 16);
+        return hexadecimal;
+    }
+    public String hexadecimal_a_binario(String hexadecimal){
+        int tamanio = hexadecimal.length();
+        char car[] = hexadecimal.toCharArray();
+        int arreglo[] = new int[tamanio];
+        for (int i = 0; i < tamanio; i++) {
+            switch(car[i]){
+                case 'F':
+                    arreglo[i] = 15;
+                    break;
+                case 'E':
+                    arreglo[i] = 14;
+                    break;
+                case 'D':
+                    arreglo[i] = 13;
+                    break;
+                case 'C':
+                    arreglo[i] = 12;
+                    break;
+                case 'B':
+                    arreglo[i] = 11;
+                    break;
+                case 'A':
+                    arreglo[i] = 10;
+                    break;
+                case '9':
+                    arreglo[i] = 9;
+                    break;
+                case '8':
+                    arreglo[i] = 8;
+                    break;
+                case '7':
+                    arreglo[i] = 7;
+                    break;
+                case '6':
+                    arreglo[i] = 6;
+                    break;
+                case '5':
+                    arreglo[i] = 5;
+                    break;
+                case '4':
+                    arreglo[i] = 4;
+                    break;
+                case '3':
+                    arreglo[i] = 3;
+                    break;
+                case '2':
+                    arreglo[i] = 2;
+                    break;
+                case '1':
+                    arreglo[i] = 1;
+                    break;
+                case '0':
+                    arreglo[i] = 0;
+                    break;
+                }
+        }
+        int potencia = 0;
+        int suma = 0;
+        System.out.println("datos del arreglo:");
+        for (int j = 0; j < tamanio; j++) {
+            potencia = (j - (tamanio-1)) * -1;
+            suma += (int) (arreglo[j] * Math.pow(16, potencia));
+        }
+        Hexadecimal he = new Hexadecimal();
+        String retorno = he.decimal_a_otro(suma, 2);
+        return retorno;
     }
     public String hexadecimal_a_decimal(String hexadecimal){
         int tamanio = hexadecimal.length();
@@ -194,5 +394,72 @@ public class Hexadecimal {
         }
         String decimal = suma + "";
         return decimal;
+    }
+    public String hexadecimal_a_octal(String hexadecimal){
+        int tamanio = hexadecimal.length();
+        char car[] = hexadecimal.toCharArray();
+        int arreglo[] = new int[tamanio];
+        for (int i = 0; i < tamanio; i++) {
+            switch(car[i]){
+                case 'F':
+                    arreglo[i] = 15;
+                    break;
+                case 'E':
+                    arreglo[i] = 14;
+                    break;
+                case 'D':
+                    arreglo[i] = 13;
+                    break;
+                case 'C':
+                    arreglo[i] = 12;
+                    break;
+                case 'B':
+                    arreglo[i] = 11;
+                    break;
+                case 'A':
+                    arreglo[i] = 10;
+                    break;
+                case '9':
+                    arreglo[i] = 9;
+                    break;
+                case '8':
+                    arreglo[i] = 8;
+                    break;
+                case '7':
+                    arreglo[i] = 7;
+                    break;
+                case '6':
+                    arreglo[i] = 6;
+                    break;
+                case '5':
+                    arreglo[i] = 5;
+                    break;
+                case '4':
+                    arreglo[i] = 4;
+                    break;
+                case '3':
+                    arreglo[i] = 3;
+                    break;
+                case '2':
+                    arreglo[i] = 2;
+                    break;
+                case '1':
+                    arreglo[i] = 1;
+                    break;
+                case '0':
+                    arreglo[i] = 0;
+                    break;
+                }
+        }
+        int potencia = 0;
+        int suma = 0;
+        System.out.println("datos del arreglo:");
+        for (int j = 0; j < tamanio; j++) {
+            potencia = (j - (tamanio-1)) * -1;
+            suma += (int) (arreglo[j] * Math.pow(16, potencia));
+        }
+        Hexadecimal he = new Hexadecimal();
+        String retorno = he.decimal_a_otro(suma, 8);
+        return retorno;
     }
 }
